b30629734a58c0f1eb9f396edeb528d3ecf82160
aimeepham14 <aimee.pham14@gmail.com>
2022-08-23 10:40:38 -0700

    computer random sequence added

--- index.js
+++ index.js
@@ -7,8 +7,23 @@ let round = 0;
 // selecting the start button and creating a new start game function when the button is clicked
 
 const startButton = document.querySelector('#start');
-const counterContainer = document.querySelector('.player-container')
+const counterContainer = document.querySelector('.counter-container')
+// THE PLAYER'S TURN
+const heading = document.querySelector('.player-container');
 
+//Creating restart button
+function restartGame(text) {
+    computerSequence = [];
+    playerSequence = [];
+    round = 0;
+    counterContainer.classList.add('dont-click');
+}
+
+//to indicate that the computer is finished and it's the player's turn
+function playerTurn(round) {
+    counterContainer.classList.remove('dont-click');
+    // info.textContent = `Your turn: ${round}' Tap${round > 1 ? 's' : ''}`;
+}
 
 // making the computer creating random combinations for the sequence
 
@@ -42,24 +57,21 @@ function nextStep() {
 function nextRound() {
     round += 1;
 
+
 //adding to the counter after each round
 counterContainer.classList.add('dont-click');
-heading.textContent = `Round ${round} of 20`;
-    const nextSequence = [...computerSequence];
-    nextSequence.push(nextStep());
-    playRound(nextSequence);
+
+heading.textContent = `Round ${round}`;
+
+const nextSequence = [...computerSequence];
+nextSequence.push(nextStep());
+playRound(nextSequence);
 
 //adding a delay after the computer is done with the sequence so the player can execute the sequence
-    sequence = [...playerSequence];
-    setTimeout(() => {
-        playerTurn(round);
-    }, round * 600 + 1000);
-}
-
-//activate when the start button is clicked
-
-function startGame() {
-    nextRound();
+computerSequence = [...nextSequence];
+ setTimeout(() => {
+    playerTurn(round);
+}, round * 600 + 1000);
 }
 
 //function to push tile value to the player sequence and stores the index. If they are equal, the round is over and the next round can begin
@@ -68,6 +80,12 @@ function handleClick(tile) {
     const index = playerSequence.push(tile) - 1;
     
     const remainingClicks = computerSequence.length - playerSequence.length;
+
+//restart game if the player does not match computer
+    if(playerSequence[index] !== computerSequence[index]) {
+        restartGame();
+        return;
+    }
 
     if (playerSequence.length === computerSequence.length) {
         playerSequence = [];
@@ -83,23 +101,19 @@ function handleClick(tile) {
     }`;
 }
 
+//activate when the start button is clicked
 
+function startGame() {
+    nextRound();
+}
 
 startButton.addEventListener('click', startGame);
 //detecting whehter to move to the next round or end game
 counterContainer.addEventListener('click', event => {
     const { tile } = event.target.dataset;
+
     if (tile) handleClick(tile);
 });
-
-// THE PLAYER'S TURN
-const heading = document.querySelector('.player-container');
-
-//to indicate that the computer is finished and it's the player's turn
-function playerTurn(round) {
-    counterContainer.classList.remove('dont-click');
-    info.TextContent = `Your turn: ${round}' Tap${round > 1 ? 's' : ''}`;
-}
 
 
 
